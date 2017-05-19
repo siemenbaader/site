f2ba51332675446e356799f7f7d08839a2e21ee5
Siemen Baader <siemenbaader@gmail.com>
2017-05-19 15:40:26 +0200

    Added styling and unit column to 'final order' as well. Set order cycle to auto close on mon morning.

--- foodclub/foodclub.js
+++ foodclub/foodclub.js
@@ -1,5 +1,52 @@
+// Namespace
 window.Lp = {};
 Lp.Foodclub = {};
+
+// Class definitions
+
+// I represent a listing of items such as user invoices or the order sent to farmer
+Lp.Foodclub.LineItemListing = {}; 
+Lp.Foodclub.LineItemListing.selector = 'abstract';
+Lp.Foodclub.LineItemListing.installUnitColumn = function(){ 
+	var self = this;
+
+	// TH insertion
+	$(self.selector).find('th.DescriptionCol').before('<th class="UnitCol">Unit</td>');
+
+	// Unit regex
+	unitRegex = /\((.+)\)$/ ;
+
+	// Unit Insertion
+	$(self.selector).find('td.DescriptionCol').each(function(index, element){
+	  var unit = element.innerHTML.match(unitRegex)[1];
+	  var fieldContent = element.innerHTML.replace(unitRegex, '').trim();
+	  var unitField = $('<td class="UnitCol">' + unit + '</td>');
+	  $(element).before(unitField);
+	  
+	  // Trim unit out of description
+	  element.innerHTML = fieldContent;
+	})
+};
+
+Lp.Foodclub.UserInvoice = { 
+	__proto__	: Lp.Foodclub.LineItemListing,
+	selector	: '.invoiceUserTable'
+};
+
+Lp.Foodclub.UserInvoice.addDeliveryDayHeader = function() {
+	var deliveryDayHeader = $('<h1>Onsdag</h1>');
+	$(document).ready(function(){
+		$('.normalPrintInvoices .invoiceUserDiv').prepend(deliveryDayHeader);
+	})
+};
+
+Lp.Foodclub.ProducerOrder = { 
+	__proto__	: Lp.Foodclub.LineItemListing,
+	selector	: '#finalOrderTable'
+};
+
+Lp.Foodclub.ProducerOrder.add
+
 
 // Private
 Lp.Foodclub.scriptTag = function( url ) {
@@ -29,50 +76,13 @@ Lp.Foodclub.installIframeResizer = function(){
 	$(document.head).append( this.iframeResizerScriptTag() );
 };
 
-Lp.Foodclub.installInvoiceUnitColum = function(){ 
-
-	$(document).ready(function(){
-
-		// TH insertion
-		$('.invoiceUserTable').find('th.DescriptionCol').before('<th class="UnitCol">Unit</td>');
-
-		// Unit regex
-		unitRegex = /\((.+)\)$/ ;
-
-		// Unit Insertion
-		$('.invoiceUserTable').find('td.DescriptionCol').each(function(index, element){
-		  
-		    
-		  var unit = element.innerHTML.match(unitRegex)[1];
-		  var fieldContent = element.innerHTML.replace(unitRegex, '').trim();
-		  
-		  
-		  var unitField = $('<td class="UnitCol">' + unit + '</td>');
-		                    
-		                    
-		  $(element).before(unitField);
-		  
-		  // Trim unit out of description
-		  element.innerHTML = fieldContent;
-		  
-		})
-	})
-};
-
-Lp.Foodclub.addDeliveryDayHeader = function(){
-	var deliveryDayHeader = $('<h1>Onsdag</h1>');
-	$(document).ready(function(){
-		$('.normalPrintInvoices .invoiceUserDiv').prepend(deliveryDayHeader);
-	})
-};
-
 
 Lp.Foodclub.initialize = function(){
 	this.installIframeResizer();
-	this.installInvoiceUnitColum();
-	this.addDeliveryDayHeader();
+	$(document).ready(function(){ Lp.Foodclub.UserInvoice.installUnitColumn(); });
+	$(document).ready(function(){ Lp.Foodclub.UserInvoice.addDeliveryDayHeader(); });
+  $(document).ready(function(){ Lp.Foodclub.ProducerOrder.installUnitColumn(); });
 };
 
 // main()
 Lp.Foodclub.initialize();
-
